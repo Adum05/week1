b7de9750ab6a98e4b5918f27f2f5a09a2aa67de9
Aday Munoz <amunozguedes@york.edu>
2024-01-28 17:03:57 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -12,3 +12,26 @@ let _ = require(`lodash`);
 let myCamelCase= `las palmas`
 
 console.log(_.camelCase(myCamelCase))
+
+let mycap= 'las palmas'
+console.log(_.capitalize(mycap));
+
+let mydivide1= 48
+let mydivide2= 4
+console.log(_.divide(mydivide1, mydivide2));
+
+let nums= [2,4,6,8,9]
+let result= _.find(nums, function (n) {
+    if (n<6) {
+        return true;
+    }
+});
+console.log(result);
+
+let mymultiply1= 32
+let mymultiply2= 56 
+console.log(_.multiply(mymultiply1, mymultiply2));
+
+let mymax= [1,56,1485,63162461,24]
+console.log(_.max(mymax));
+
